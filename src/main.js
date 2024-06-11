713eee71640b110bab622e627a4209d6e6733fd4
Aesirlvu <you@example.com>
2024-06-10 23:06:13 -0300

    routes

--- src/main.js
+++ src/main.js
@@ -1,66 +1,21 @@
+// import mysql from "mysql2/promise"
 import express from "express";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "../src/config/db.js";
+import { booksRouter } from "../src/routes/books.js";
 
-const PORT = 3300;
+const PORT = process.env.PORT ?? 3300;
+const URL = "http://localhost:";
 const app = express();
 
 app.use(express.json());
 
-app.get("/books", async (req, res) => {
-  const connection = await connectDB();
-  try {
-    const result = await connection.query(`
-        SELECT * FROM books
-        `);
-    res.json(result[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error.");
-  } finally {
-    connection.end();
-  }
-});
+const startServer = async () => {
+  await connectDB();
 
-app.post("/books", async (req, res) => {
-  const connection = await connectDB();
-  try {
-    const { title, author } = req.body;
-    const query = `
-    INSERT INTO books (title, author) VALUES
-    (?, ?)
-    `;
-    const checkQuery = `
-    SELECT * FROM books WHERE title = ? AND author = ?
-    `;
-    const existingBooks = await connection.query(checkQuery, [title, author]);
-    if (existingBooks.length > 0) {
-      return res.status(400).send("El libro ya existe.");
-    }
-    const result = await connection.query(query, [title, author]);
-    res.send("Se creó");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Hubo un error.");
-  }
-});
+  app.use("/books", booksRouter);
 
-app.delete("/", async (req, res) => {
-  const connection = await connectDB();
-  try {
-    const { id } = req.body;
-    const queryDelete = `
-    DELETE FROM books WHERE id = ?
-    `;
-    const result = await connection.query(queryDelete, [id]);
-    res.send("Registro eliminado.");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("No se pudo eliminar");
-  } finally {
-    connection.end();
-  }
-});
-
-app.listen(PORT, (res) => {
-  console.log(`Servidor escuchando en: ${PORT}`);
-});
+  app.listen(PORT, (res) => {
+    console.log(`Servidor escuchando en: ${URL}${PORT}/books`);
+  });
+};
+startServer();
